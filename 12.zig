aefab3230af157e83c7b6db33be91a79e40b7abe
Daniel MacDougall <dmacdougall@gmail.com>
2023-12-12 01:39:04 -0800

    Day 12 (Part 2 WIP)

--- 12.zig
+++ 12.zig
@@ -11,28 +11,97 @@ const Line = struct {
 };
 
 pub fn main() !void {
-    const lines = try util.file_lines(allocator, "12.txt");
-    var sum: i64 = 0;
+    var perm = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const lines = try util.file_lines(perm.allocator(), "12.txt");
+    var part1: i64 = 0;
+    var part2: i64 = 0;
     for (lines) |line| {
-        const l = try parse(line);
-        // std.debug.print("{s} {d}\n", .{ l.s, l.ns });
-        sum += try solve(l, 0);
+        const l1 = try parse(line);
+        part1 += try solve(l1, 0);
+        const l2 = try unfold(l1);
+        // std.debug.print("{s} {d}\n", .{ l2.s, l2.ns });
+        part2 += try solve(l2, 0);
+        _ = arena.reset(.free_all);
     }
-    std.debug.print("{d}\n", .{sum});
+    std.debug.print("{d}\n", .{part1});
+    std.debug.print("{d}\n", .{part2});
+}
+
+fn unfold(line: Line) !Line {
+    const s = try allocator.alloc(u8, line.s.len * 5 + 4);
+    var li: usize = 0;
+    for (s, 0..) |_, si| {
+        if (li == line.s.len) {
+            s[si] = '?';
+            li = 0;
+        } else {
+            s[si] = line.s[li];
+            li += 1;
+        }
+    }
+    var ns = std.ArrayList(usize).init(allocator);
+    for (0..5) |_| {
+        try ns.appendSlice(line.ns);
+    }
+    const vns = try ns.toOwnedSlice();
+    const l = Line{
+        .s = s,
+        .ns = vns,
+        .vns = vns,
+    };
+    return l;
 }
 
 fn solve(line: Line, from: usize) !i64 {
     // std.debug.print("{s} {d} {d}\n", .{ line.s, line.ns, from });
+    // for (0..from) |_| {
+    //     std.debug.print(" ", .{});
+    // }
+    // std.debug.print("^\n", .{});
+
     if (line.ns.len == 0) {
         if (try verify(line)) {
             return 1;
         }
         return 0;
     }
+
+    var rem_groups: usize = 0;
+    var rem_groups_it = std.mem.tokenizeAny(u8, line.s[from..], ".");
+    while (rem_groups_it.next()) |s| {
+        // std.debug.print("{s} ", .{s});
+        if (std.mem.containsAtLeast(u8, s, 1, "#")) {
+            rem_groups += 1;
+        }
+    }
+    // std.debug.print("\n\n", .{});
+    if (rem_groups > line.ns.len) {
+        return 0;
+    }
+
     const n = line.ns[0];
     var sum: i64 = 0;
+    var rest: usize = line.ns.len - 1;
+    for (line.ns) |m| {
+        rest += m;
+    }
+    if (from + rest > line.s.len) {
+        return 0;
+    }
     var i: usize = from;
-    while (i <= line.s.len - n) : (i += 1) {
+    while (i + n <= line.s.len) : (i += 1) {
+        if (i > 0 and line.s[i - 1] == '#') {
+            if (line.s[i] == '#') {
+                return sum;
+            }
+            continue;
+        }
+        if (i + n < line.s.len and line.s[i + n] == '#') {
+            if (line.s[i] == '#') {
+                return sum;
+            }
+            continue;
+        }
         var place = true;
         var j: usize = i;
         while (j < i + n) : (j += 1) {
@@ -42,13 +111,25 @@ fn solve(line: Line, from: usize) !i64 {
             }
         }
         if (!place) {
+            // if (std.mem.containsAtLeast(u8, line.s[i .. i + n], 1, "#")) {
+            //     return sum;
+            // }
+            if (line.s[i] == '#') {
+                return sum;
+            }
             continue;
+        }
+        if (rem_groups == line.ns.len and !std.mem.containsAtLeast(u8, line.s[i .. i + n], 1, "#")) {
+            return sum;
         }
         var s = try allocator.alloc(u8, line.s.len);
         std.mem.copyForwards(u8, s, line.s);
         j = i;
         while (j < i + n) : (j += 1) {
             s[j] = '#';
+        }
+        if (i > 0 and line.s[i - 1] == '?') {
+            s[i - 1] = '.';
         }
         if (i + n < s.len and s[i + n] == '?') {
             s[i + n] = '.';
